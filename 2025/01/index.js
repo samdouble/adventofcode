50bf6fece7fd1ca58321882470d65e1c2328ca13
samdouble <samleafs@protonmail.com>
2025-12-04 07:55:36 -0500

    feat(2025.01-2): completed challenge

--- 2025/01/index.js
+++ 2025/01/index.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const lines = input.split('\n');
 
-console.log(lines.length);
 const INITIAL_POS = 50;
 const MIN_POS = 0;
 const MAX_POS = 99;
@@ -11,15 +10,36 @@ const NB_POSITIONS = MAX_POS - MIN_POS + 1;
 
 const rotate = (pos, direction, distance) => {
     if (direction === 'R') {
-        return (pos + distance) % NB_POSITIONS;
+        let newPos = pos + distance;
+        let nbClickedOn0 = 0;
+        while (newPos > MAX_POS) {
+            newPos -= NB_POSITIONS;
+            nbClickedOn0++;
+        }
+        return {
+            newPos: newPos % NB_POSITIONS,
+            nbClickedOn0,
+        };
     }
     if (direction === 'L') {
-        return (pos - distance + NB_POSITIONS) % NB_POSITIONS;
+        let nbClickedOn0 = 0;
+        if (pos === MIN_POS) {
+            nbClickedOn0--;
+        }
+        let newPos = pos - distance;
+        while (newPos <= MIN_POS) {
+            newPos += NB_POSITIONS;
+            nbClickedOn0++;
+        }
+        return {
+            newPos: newPos % NB_POSITIONS,
+            nbClickedOn0: nbClickedOn0 % NB_POSITIONS,
+        };
     }
     throw new Error('Invalid direction');
 };
 
-const getPassword = (rotations) => {
+const getPassword = (rotations, countEveryClickOn0 = false) => {
     let currentPosition = INITIAL_POS;
     let password = 0;
 
@@ -27,16 +47,22 @@ const getPassword = (rotations) => {
         const direction = rotation.slice(0, 1);
         const distance = parseInt(rotation.slice(1));
 
-        currentPosition = rotate(currentPosition, direction, distance);
+        const { newPos, nbClickedOn0 } = rotate(currentPosition, direction, distance);
+        currentPosition = newPos;
 
-        if (currentPosition === 0) {
+        if (countEveryClickOn0) {
+            password += nbClickedOn0;
+        } else if (!countEveryClickOn0 && currentPosition === 0) {
             password++;
         }
     }
     return password;
 }
 
+// Part 1
 console.log(getPassword(lines));
+// Part 2
+console.log(getPassword(lines, true));
 
 module.exports = {
     rotate,
--- 2025/01/test.js
+++ 2025/01/test.js
@@ -2,9 +2,30 @@ const { getPassword, rotate } = require('./index');
 
 describe('rotate', () => {
     it('should rotate correctly for given direction and distance', () => {
-        expect(rotate(50, 'R', 68)).toBe(18);
-        expect(rotate(50, 'L', 30)).toBe(20);
-        expect(rotate(50, 'R', 48)).toBe(98);
+        expect(rotate(50, 'R', 68)).toEqual({ newPos: 18, nbClickedOn0: 1 });
+        expect(rotate(50, 'L', 30)).toEqual({ newPos: 20, nbClickedOn0: 0 });
+        expect(rotate(50, 'R', 48)).toEqual({ newPos: 98, nbClickedOn0: 0 });
+        expect(rotate(50, 'L', 100)).toEqual({ newPos: 50, nbClickedOn0: 1 });
+        expect(rotate(50, 'L', 200)).toEqual({ newPos: 50, nbClickedOn0: 2 });
+        expect(rotate(50, 'R', 1000)).toEqual({ newPos: 50, nbClickedOn0: 10 });
+        expect(rotate(50, 'R', 149)).toEqual({ newPos: 99, nbClickedOn0: 1 });
+        expect(rotate(50, 'R', 150)).toEqual({ newPos: 0, nbClickedOn0: 2 });
+        expect(rotate(50, 'R', 151)).toEqual({ newPos: 1, nbClickedOn0: 2 });
+        expect(rotate(50, 'L', 149)).toEqual({ newPos: 1, nbClickedOn0: 1 });
+        expect(rotate(50, 'L', 150)).toEqual({ newPos: 0, nbClickedOn0: 2 });
+        expect(rotate(50, 'L', 151)).toEqual({ newPos: 99, nbClickedOn0: 2 });
+    });
+    it('Example 2', () => {
+        expect(rotate(50, 'L', 68)).toEqual({ newPos: 82, nbClickedOn0: 1 });
+        expect(rotate(82, 'L', 30)).toEqual({ newPos: 52, nbClickedOn0: 0 });
+        expect(rotate(52, 'R', 48)).toEqual({ newPos: 0, nbClickedOn0: 1 });
+        expect(rotate(0, 'L', 5)).toEqual({ newPos: 95, nbClickedOn0: 0 });
+        expect(rotate(95, 'R', 60)).toEqual({ newPos: 55, nbClickedOn0: 1 });
+        expect(rotate(55, 'L', 55)).toEqual({ newPos: 0, nbClickedOn0: 1 });
+        expect(rotate(0, 'L', 1)).toEqual({ newPos: 99, nbClickedOn0: 0 });
+        expect(rotate(99, 'L', 99)).toEqual({ newPos: 0, nbClickedOn0: 1 });
+        expect(rotate(0, 'R', 14)).toEqual({ newPos: 14, nbClickedOn0: 0 });
+        expect(rotate(14, 'L', 82)).toEqual({ newPos: 32, nbClickedOn0: 1 });
     });
 });
 
@@ -25,5 +46,23 @@ describe('getPassword', () => {
 
         const result = getPassword(lines);
         expect(result).toBe(3);
+    });
+
+    it('Example 2', () => {
+        const lines = [
+            'L68',
+            'L30',
+            'R48',
+            'L5',
+            'R60',
+            'L55',
+            'L1',
+            'L99',
+            'R14',
+            'L82',
+        ];
+
+        const result = getPassword(lines, true);
+        expect(result).toBe(6);
     });
 });
