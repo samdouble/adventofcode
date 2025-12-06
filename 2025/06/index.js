7378839b66dc10629245a12bdcf69170220e75f4
samdouble <samleafs@protonmail.com>
2025-12-06 01:01:41 -0500

    feat(2025.06-2): completed challenge

--- 2025/06/index.js
+++ 2025/06/index.js
@@ -3,42 +3,73 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const rows = input.split('\n');
 
-const parseInput = (rows) => {
+const parseInput = (rows, cephalopod = false) => {
     const equations = [];
-    const nbEquations = rows[0].split(/\s+/).length;
-    for (let i = 0; i < nbEquations; i++) {
-        equations.push(rows.map(row => row.split(/\s+/)[i]));
+    if (cephalopod) {
+        const splits = [];
+        const chars = rows.map(row => row.split(''));
+        for (let i = 0; i < chars[0].length; i++) {
+            if (chars.every(char => char[i] === ' ')) {
+                splits.push(i);
+            }
+        }
+        for (let i = 0; i < splits.length; i++) {
+            if (i === 0) {
+                equations.push(rows.map(row => row.slice(0, splits[i])));
+            } else {
+                equations.push(rows.map(row => row.slice(splits[i - 1] + 1, splits[i])));
+            }
+            if (i === splits.length - 1) {
+                equations.push(rows.map(row => row.slice(splits[i] + 1)));
+            }
+        }
+    } else {
+        const trimmedRows = rows.map(row => row.trim());
+        const nbEquations = trimmedRows[0].split(/\s+/).length;
+        for (let i = 0; i < nbEquations; i++) {
+            equations.push(trimmedRows.map(row => row.split(/\s+/)[i]));
+        }
     }
     return equations;
 };
 
-const evaluateEquation = (equation) => {
-    let operator = equation[equation.length - 1];
-    let operands = equation.slice(0, -1).map(operand => parseInt(operand));
+const evaluateEquation = (equation, cephalopod = false) => {
+    let operator = equation[equation.length - 1].trim();
+    let operands = equation.slice(0, -1);
     const initialValue = operator === '+' ? 0 : 1;
+    if (cephalopod) {
+        const newOperands = []
+        for (let i = 0; i < operands[0].toString().length; i++) {
+            newOperands.push(parseInt(operands.map(operand => operand.toString()[i]).join('')));
+        }
+        operands = newOperands;
+    }
     return operands.reduce((acc, operand) => {
         if (operator === '+') {
-            return acc + operand;
+            return acc + parseInt(operand);
         } else if (operator === '*') {
-            return acc * operand;
+            return acc * parseInt(operand);
         }
         throw new Error('Invalid operator');
     }, initialValue);
 }
 
-const getResult = (rows) => {
-    const equations = parseInput(rows);
+const getResult = (rows, cephalopod = false) => {
+    const equations = parseInput(rows, cephalopod);
     let totalResult = 0;
     for (const equation of equations) {
-        totalResult += evaluateEquation(equation);
+        totalResult += evaluateEquation(equation, cephalopod);
     }
     return totalResult;
 }
 
 // Part 1
 console.log(getResult(rows)); // 1st try, 19 pts
+// Part 2
+console.log(getResult(rows, true)); // 
 
 module.exports = {
     getResult,
-    evaluateEquation
+    evaluateEquation,
+    parseInput,
 };
--- 2025/06/test.js
+++ 2025/06/test.js
@@ -1,4 +1,4 @@
-const { getResult, evaluateEquation } = require('./index');
+const { parseInput, evaluateEquation } = require('./index');
 
 describe('evaluateEquation', () => {
     it('Example 1', () => {
@@ -6,5 +6,22 @@ describe('evaluateEquation', () => {
         expect(evaluateEquation(['328', '64', '98', '+'])).toEqual(490);
         expect(evaluateEquation(['51', '387', '215', '*'])).toEqual(4243455);
         expect(evaluateEquation(['64', '23', '314', '+'])).toEqual(401);
+    });
+    it('Example 2', () => {
+        expect(evaluateEquation(['123', ' 45', '  6', '*  '], true)).toEqual(8544);
+        expect(evaluateEquation(['328', '64 ', '98 ', '+  '], true)).toEqual(625);
+        expect(evaluateEquation([' 51', '387', '215', '*  '], true)).toEqual(3253600);
+        expect(evaluateEquation(['64 ', '23 ', '314', '+  '], true)).toEqual(1058);
+    });
+});
+
+describe('parseInput', () => {
+    it('Example 2', () => {
+        expect(parseInput(['123 328  51 64 ', ' 45 64  387 23 ', '  6 98  215 314', '*   +   *   +  '], true)).toEqual([
+            ['123', ' 45', '  6', '*  '],
+            ['328', '64 ', '98 ', '+  '],
+            [' 51', '387', '215', '*  '],
+            ['64 ', '23 ', '314', '+  '],
+        ]);
     });
 });
