f73cf7dcedf5505d430d731340203512e6584363
samdouble <samleafs@protonmail.com>
2025-12-07 11:34:29 -0500

    feat(2025.07): working, but out of memory

--- 2025/07/index.js
+++ 2025/07/index.js
@@ -3,6 +3,32 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const rows = input.split('\n');
 
+const getPossibilitiesForBeam = (position, beamRows) => {
+    const possibilities = [];
+    if (beamRows.length === 0) {
+        possibilities.push([position]);
+    }
+    else if (beamRows[0][position] === '^') {
+        for (const possibility of getPossibilitiesForBeam(position - 1, beamRows.slice(1))) {
+            possibilities.push([position - 1, ...possibility]);
+        }
+        for (const possibility of getPossibilitiesForBeam(position + 1, beamRows.slice(1))) {
+            possibilities.push([position + 1, ...possibility]);
+        }
+    }
+    else if (beamRows[0][position] === '.') {
+        for (const possibility of getPossibilitiesForBeam(position, beamRows.slice(1))) {
+            possibilities.push([position, ...possibility]);
+        }
+    }
+    return possibilities;
+}
+
+const countQuantumSplits = (rows) => {
+    const initialPosition = rows[0].indexOf('S');
+    return getPossibilitiesForBeam(initialPosition, rows.slice(1)).length;
+}
+
 const countSplits = (rows) => {
     const initialPosition = rows[0].indexOf('S');
     let nbBeamSplits = 0;
@@ -24,7 +50,10 @@ const countSplits = (rows) => {
 
 // Part 1
 console.log(countSplits(rows)); // 1st try, N pts
+// Part 2
+console.log(countQuantumSplits(rows)); // 1st try, N pts
 
 module.exports = {
     countSplits,
+    countQuantumSplits,
 };
