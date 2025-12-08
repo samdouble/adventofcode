dc003dc48b703bcad41f30f8cf320a77b3d7d04b
samdouble <samleafs@protonmail.com>
2025-12-08 08:30:27 -0500

    feat(2025.08-2): completed challenge

--- 2025/08/index.js
+++ 2025/08/index.js
@@ -7,52 +7,73 @@ const getDistance = (pt1, pt2) => {
     return Math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2 + (pt1[2] - pt2[2]) ** 2);
 }
 
-
-const getSolution = (rows, nbConnections, nbCircuits) => {
+const getDistances = (rows) => {
     const distances = new Map();
-
-    // We get the nbConnections closest pts
     for (let i = 0; i < rows.length; i++) {
         for (let j = i + 1; j < rows.length; j++) {
             const distance = getDistance(rows[i], rows[j]);
-            if (distances.size < nbConnections) {
-                distances.set([i, j], distance);
-            } else {
-                const maxDistance = Math.max(...distances.values());
-                if (distance < maxDistance) {
-                    distances.delete(distances.keys().find(key => distances.get(key) === maxDistance));
-                    distances.set([i, j], distance);
-                }
-            }
+            distances.set([i, j], distance);
         }
     }
-    // We join the junction boxes into circuits
-    const circuits = []
-    for (const [[pt1, pt2], value] of distances.entries()) {
-        const circuitPt1 = circuits.findIndex(circuit => circuit.includes(pt1));
-        const circuitPt2 = circuits.findIndex(circuit => circuit.includes(pt2));
+    return distances;
+}
+
+const makeCircuits = (distances, nbBoxes) => {
+    const circuits = [];
+    let lastConnection = null;
+
+    for (const [[pt1, pt2], value] of distances) {
+        const circuitPt1 = circuits.findIndex(circuit => circuit.has(pt1));
+        const circuitPt2 = circuits.findIndex(circuit => circuit.has(pt2));
         
         if (circuitPt1 !== -1 && circuitPt2 !== -1) {
             if (circuitPt1 !== circuitPt2) {
-                circuits[circuitPt1] = [...circuits[circuitPt1], ...circuits[circuitPt2]];
+                circuits[circuitPt1] = new Set([...circuits[circuitPt1], ...circuits[circuitPt2]]);
                 circuits.splice(circuitPt2, 1);
             }
         } else if (circuitPt1 !== -1) {
-            circuits[circuitPt1].push(pt2);
+            circuits[circuitPt1].add(pt2);
         } else if (circuitPt2 !== -1) {
-            circuits[circuitPt2].push(pt1);
+            circuits[circuitPt2].add(pt1);
         } else {
-            circuits.push([pt1, pt2]);
+            circuits.push(new Set([pt1, pt2]));
+        }
+
+        if (circuits.length === 1 && circuits[0].size === nbBoxes) {
+            lastConnection = [pt1, pt2];
+            break;
         }
     }
-    return circuits
-        .sort((a, b) => b.length - a.length)
+    return {
+        circuits,
+        lastConnection,
+    };
+}
+
+const getSolution = (rows, nbConnections, nbCircuits) => {
+    const distances = getDistances(rows);
+    const sortedDistances = Array.from(distances.entries())
+        .sort((a, b) => a[1] - b[1])
+        .slice(0, nbConnections);
+    const { circuits, lastConnection } = makeCircuits(sortedDistances, rows.length);
+    
+    const solution1 = circuits
+        .sort((a, b) => b.size - a.size)
         .slice(0, nbCircuits)
-        .reduce((acc, circuit) => acc * circuit.length, 1);
+        .reduce((acc, circuit) => acc * circuit.size, 1);
+
+    const solution2 = lastConnection ? rows[lastConnection[0]][0] * rows[lastConnection[1]][0] : null;
+
+    return {
+        solution1,
+        solution2,
+    };
 }
 
 // Part 1
-console.log(getSolution(rows, 1000, 3)); // 
+console.log(getSolution(rows, 1000, 3).solution1); // 1st try, N pts
+// Part 2
+console.log(getSolution(rows, 1000000, 1000).solution2); //
 
 module.exports = {
     getSolution,
