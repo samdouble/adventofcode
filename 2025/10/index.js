8209fc90a901abc2ae52b2578a5fd54886ce3272
samdouble <samleafs@protonmail.com>
2025-12-10 13:46:35 -0500

    feat(2025.10-2): working algo, very slow

--- 2025/10/index.js
+++ 2025/10/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const isEqual = require('lodash.isequal');
 
 const input = fs.readFileSync('input.txt', 'utf8');
 const machines = input.split('\n').map(machineStr => {
@@ -8,6 +7,8 @@ const machines = input.split('\n').map(machineStr => {
     const lights = machineStr.slice(lightsStart + 1, lightsEnd).split('').map(l => l === '#' ? 1 : 0);
 
     const joltageStart = machineStr.indexOf('{');
+    const joltageEnd = machineStr.indexOf('}');
+    const joltages = machineStr.slice(joltageStart + 1, joltageEnd).split(',').map(Number);
 
     const buttons = machineStr
         .slice(lightsEnd + 1, joltageStart)
@@ -23,68 +24,63 @@ const machines = input.split('\n').map(machineStr => {
     return {
         lights,
         buttons,
+        joltages,
     };
 });
 
-const arrayOfArrayIncludes = (arrayOfArrays, array) => {
-    return arrayOfArrays.some(a => isEqual(a, array));
-};
-
-const applyButton = (lights, button) => {
+const applyButton = (lights, button, useJoltages = false) => {
+    if (useJoltages) {
+        return lights.map((light, i) => light + button[i]);
+    }
     return lights.map((light, i) => (light + button[i]) % 2);
 };
 
-const getMinNumberPushesForMachine = (lights, buttons, currentLightsState, history, combinations) => {
-    const diff = lights.map((light, i) => Math.abs(light - currentLightsState[i]));
+const getMinNumberPushesForMachine = (desiredState, buttons, currentState, combinations, useJoltages) => {
+    const diff = desiredState.map((s, i) => Math.abs(s - currentState[i]));
 
     if (combinations[JSON.stringify(diff)]) {
         return combinations[JSON.stringify(diff)];
     }
     for (const [combination, nbPushes] of Object.entries(combinations)) {
         for (const button of buttons) {
-            const newLightsStateWithButton = applyButton(JSON.parse(combination), button);
+            const newStateWithButton = applyButton(JSON.parse(combination), button, useJoltages);
             if (
-                !combinations[JSON.stringify(newLightsStateWithButton)]
-                || combinations[JSON.stringify(newLightsStateWithButton)] > nbPushes + 1
+                !combinations[JSON.stringify(newStateWithButton)]
+                || combinations[JSON.stringify(newStateWithButton)] > nbPushes + 1
             ) {
-                combinations[JSON.stringify(newLightsStateWithButton)] = nbPushes + 1;
+                combinations[JSON.stringify(newStateWithButton)] = nbPushes + 1;
             }
         }
     }
-    return getMinNumberPushesForMachine(lights, buttons, currentLightsState, history, combinations);
-    // const sensibleButtons = buttons.filter(button => !arrayOfArrayIncludes(history, applyButton(currentLightsState, button)));
-    // if (sensibleButtons.length === 0) {
-    //     return Infinity;
-    // }
-// 
-    // return 1 + Math.min(
-    //     ...sensibleButtons.map(button => {
-    //         const newLightsState = applyButton(currentLightsState, button);
-    //         console.log("Testing button", button, newLightsState, history);
-    //         return getMinNumberPushesForMachine(lights, buttons, newLightsState, [...history, newLightsState], combinations);
-    //     })
-    // );
+    console.log(combinations);
+    return getMinNumberPushesForMachine(desiredState, buttons, currentState, combinations, useJoltages);
 };
 
-const getMinTotalNumberOfPushes = (machines) => {
+const getMinTotalNumberOfPushes = (machines, useJoltages = false) => {
     let totalNbPushes = 0;
+    let i = 0;
     for (const machine of machines) {
-        const { lights } = machine;
+        const { lights, joltages } = machine;
         const buttons = machine.buttons.map(b => (
             new Array(lights.length).fill(0).map((_, index) => b.includes(index) ? 1 : 0))
         );
-        const initialState = new Array(lights.length).fill(0);
+        const initialState = new Array(buttons[0].length).fill(0);
         const combinations = {
             [JSON.stringify(initialState)]: 0,
         };
-        const minNbPushesForMachine = getMinNumberPushesForMachine(lights, buttons, initialState, [initialState], combinations);
+        const desiredState = useJoltages ? joltages : lights;
+        const minNbPushesForMachine = getMinNumberPushesForMachine(desiredState, buttons, initialState, combinations, useJoltages);
+        console.log(i, machine, minNbPushesForMachine);
         totalNbPushes += minNbPushesForMachine;
+        i++;
     }
     return totalNbPushes;
 };
 
 // Part 1
-console.log(getMinTotalNumberOfPushes(machines)); // 1st try, N pts
+// console.log(getMinTotalNumberOfPushes(machines)); // 1st try, N pts
+// Part 2
+//console.log(getMinTotalNumberOfPushes(machines, true)); // 
 
 module.exports = {
     getMinTotalNumberOfPushes,
--- 2025/10/test.js
+++ 2025/10/test.js
@@ -8,7 +8,7 @@ describe('getSolution', () => {
                     {
                         lights: [0, 1, 1, 0],
                         buttons: [[3],[1, 3], [2], [2, 3], [0, 2], [0, 1]],
-                        joltage: [3, 5, 4, 7],
+                        joltages: [3, 5, 4, 7],
                     },
                 ]
             )
@@ -19,21 +19,57 @@ describe('getSolution', () => {
                     {
                         lights: [0, 0, 0, 1, 0],
                         buttons: [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]],
-                        joltage: [7, 5, 12, 7, 2],
+                        joltages: [7, 5, 12, 7, 2],
                     },
                 ]
             )
         ).toEqual(3);
-        // expect(
-        //     getMinTotalNumberOfPushes(
-        //         [
-        //             {
-        //                 lights: [1, 0, 0, 0, 1, 0],
-        //                 buttons: [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]],
-        //                 joltage: [10, 11, 11, 5, 10, 5],
-        //             },
-        //         ]
-        //     )
-        // ).toEqual(2);
+        expect(
+            getMinTotalNumberOfPushes(
+                [
+                    {
+                        lights: [0, 1, 1, 1, 0, 1],
+                        buttons: [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]],
+                        joltages: [10, 11, 11, 5, 10, 5],
+                    },
+                ]
+            )
+        ).toEqual(2);
+    });
+
+    it('Example 2', () => {
+        expect(
+            getMinTotalNumberOfPushes(
+                [
+                    {
+                        lights: [0, 1, 1, 0],
+                        buttons: [[3],[1, 3], [2], [2, 3], [0, 2], [0, 1]],
+                        joltages: [3, 5, 4, 7],
+                    },
+                ], true
+            )
+        ).toEqual(10);
+        expect(
+            getMinTotalNumberOfPushes(
+                [
+                    {
+                        lights: [0, 0, 0, 1, 0],
+                        buttons: [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]],
+                        joltages: [7, 5, 12, 7, 2],
+                    },
+                ], true
+            )
+        ).toEqual(12);
+        expect(
+            getMinTotalNumberOfPushes(
+                [
+                    {
+                        lights: [1, 0, 0, 0, 1, 0],
+                        buttons: [[0,1,2,3,4], [0,3,4], [0,1,2,4,5], [1,2]],
+                        joltages: [10, 11, 11, 5, 10, 5],
+                    },
+                ], true
+            )
+        ).toEqual(11);
     });
 });
