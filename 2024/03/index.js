0ef92e787869ba1c59927b67fd023889eeca8b98
samdouble <samleafs@protonmail.com>
2025-12-21 11:43:12 -0500

    feat(2024.03-2): completed challenge

--- 2024/03/index.js
+++ 2024/03/index.js
@@ -3,15 +3,24 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const memory = input;
 
-const getResult = (memory) => {
-    const re = /mul\((\d+),(\d+)\)/g;
+const getResult = (memory, considerDoInstructions = false) => {
+    const re = considerDoInstructions
+        ? /(?:do(?:n\'t)?\(\)|mul\((\d+),(\d+)\))/g
+        : /mul\((\d+),(\d+)\)/g;
     let match;
 
     let result = 0;
+    let doNotConsider = false;
     do {
         match = re.exec(memory);
         if (match) {
-            result += Number(match[1]) * Number(match[2]);
+            if (match[0].startsWith('mul') && !doNotConsider) {
+                result += Number(match[1]) * Number(match[2]);
+            } else if (match[0] === 'do()') {
+                doNotConsider = false;
+            } else if (match[0] === "don't()") {
+                doNotConsider = considerDoInstructions;
+            }
         }
     } while (match);
     return result;
@@ -19,6 +28,8 @@ const getResult = (memory) => {
 
 // Part 1
 console.log(getResult(memory));
+// Part 2
+console.log(getResult(memory, true));
 
 module.exports = {
     getResult,
--- 2024/03/test.js
+++ 2024/03/test.js
@@ -4,4 +4,7 @@ describe('getNbSafeReports', () => {
     it('Example 1', () => {
         expect(getResult('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))')).toEqual(161);
     });
+    it('Example 2', () => {
+        expect(getResult("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", true)).toEqual(48);
+    });
 });
