9fa80964def58a5f9bd49df5ddc769ad830e6543
samdouble <samleafs@protonmail.com>
2025-12-21 09:28:15 -0500

    feat(2024.02-2): completed challenge

--- 2024/02/index.js
+++ 2024/02/index.js
@@ -7,29 +7,34 @@ const isBetween = (value, min, max) => {
     return value >= min && value <= max;
 };
 
-const isReportSafe = (report) => {
+const isReportSafe = (report, tolerate1BadLevel = false) => {
     if (report.length <= 1) {
         return true;
     }
     const ascending = report[1] - report[0] > 0;
     for (let i = 0; i < report.length - 1; i++) {
-        if (ascending) {
-            if (!isBetween(report[i+1] - report[i], 1, 3)) {
+        if (
+            (ascending && !isBetween(report[i+1] - report[i], 1, 3))
+            || (!ascending && !isBetween(report[i] - report[i+1], 1, 3))
+        ) {
+            if (!tolerate1BadLevel) {
                 return false;
-            }
-        } else {
-            if (!isBetween(report[i] - report[i+1], 1, 3)) {
-                return false;
+            } else {
+                return (
+                    isReportSafe(report.filter((_, index) => index !== i - 1))
+                    || isReportSafe(report.filter((_, index) => index !== i))
+                    || isReportSafe(report.filter((_, index) => index !== i + 1))
+                );
             }
         }
     }
     return true;
 }
 
-const getNbSafeReports = (reports) => {
+const getNbSafeReports = (reports, tolerate1BadLevel = false) => {
     let nbSafeReports = 0;
     for (const report of reports) {
-        if (isReportSafe(report)) {
+        if (isReportSafe(report, tolerate1BadLevel)) {
             nbSafeReports++;
         }
     }
@@ -38,6 +43,8 @@ const getNbSafeReports = (reports) => {
 
 // Part 1
 console.log(getNbSafeReports(reports));
+// Part 2
+console.log(getNbSafeReports(reports, true));
 
 module.exports = {
     isReportSafe,
--- 2024/02/test.js
+++ 2024/02/test.js
@@ -11,10 +11,33 @@ describe('getNbSafeReports', () => {
             [1, 3, 6, 7, 9],
         ])).toEqual(2);
     });
+    it('Example 2', () => {
+        expect(getNbSafeReports([
+            [7, 6, 4, 2, 1],
+            [1, 2, 7, 8, 9],
+            [9, 7, 6, 2, 1],
+            [1, 3, 2, 4, 5],
+            [8, 6, 4, 4, 1],
+            [1, 3, 6, 7, 9],
+        ], true)).toEqual(4);
+    });
 });
 
 describe('isReportSafe', () => {
     it('Example 1', () => {
         expect(isReportSafe([7, 6, 4, 2, 1])).toEqual(true);
+        expect(isReportSafe([1, 2, 7, 8, 9])).toEqual(false);
+        expect(isReportSafe([9, 7, 6, 2, 1])).toEqual(false);
+        expect(isReportSafe([1, 3, 2, 4, 5])).toEqual(false);
+        expect(isReportSafe([8, 6, 4, 4, 1])).toEqual(false);
+        expect(isReportSafe([1, 3, 6, 7, 9])).toEqual(true);
+    });
+    it('Example 2', () => {
+        expect(isReportSafe([7, 6, 4, 2, 1], true)).toEqual(true);
+        expect(isReportSafe([1, 2, 7, 8, 9], true)).toEqual(false);
+        expect(isReportSafe([9, 7, 6, 2, 1], true)).toEqual(false);
+        expect(isReportSafe([1, 3, 2, 4, 5], true)).toEqual(true);
+        expect(isReportSafe([8, 6, 4, 4, 1], true)).toEqual(true);
+        expect(isReportSafe([1, 3, 6, 7, 9], true)).toEqual(true);
     });
 });
