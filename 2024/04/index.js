a24e783b1bdcb975b3a85b2575b5e86e5dd91d50
samdouble <samleafs@protonmail.com>
2025-12-22 21:30:07 -0500

    feat(2024.04): completed challenge

--- 2024/04/index.js
+++ 2024/04/index.js
@@ -32,18 +32,67 @@ const countWord = (lines, word, adjacent) => {
     // Vertically
     const columns = lines[0].split('')
         .map((_c, index) => {
-            return lines.reduce((acc, l) => acc + l.split('')[index], [])
+            return lines.reduce((acc, l) => acc + l[index], [])
         });
     for (const column of columns) {
         count += countWordOnLine(column.split(''), word, adjacent);
         // Reverse
         count += countWordOnLine(column.split(''), word.split('').reverse().join(''), adjacent);
     }
+    // Diagonally SW-NE
+    const diagonalsSWNE = [];
+    for (let i = 0; i < lines.length; i++) {
+        let diagonal = '';
+        for (let j = 0; j <= Math.min(i, lines[i].length); j++) {
+            diagonal += lines[i - j][j];
+        }
+        diagonalsSWNE.push(diagonal);
+    }
+    for (let i = 1; i < lines[lines.length - 1].length; i++) {
+        let diagonal = '';
+        for (let j = Math.min(lines.length, lines[i].length - 1); j >= 1; j--) {
+            if (lines[i].length - j - 1 + i > lines[j].length - 1) {
+                break;
+            }
+            diagonal += lines[j][lines[i].length - j - 1 + i];
+        }
+        diagonalsSWNE.push(diagonal);
+    }
+    for (const diagonal of diagonalsSWNE) {
+        count += countWordOnLine(diagonal.split(''), word, adjacent);
+        // Reverse
+        count += countWordOnLine(diagonal.split(''), word.split('').reverse().join(''), adjacent);
+    }
+    // Diagonally NW - SE
+    const diagonalsNWSE = [];
+    //(0, 0)(1, 1)
+    for (let i = 0; i < lines.length; i++) {
+        let diagonal = '';
+        for (let j = 0; j <= Math.min(lines.length - 1 - i, lines[i].length - 1); j++) {
+            diagonal += lines[j + i][j];
+        }
+        diagonalsNWSE.push(diagonal);
+    }
+    for (let i = 1; i < lines[0].length; i++) {
+        let diagonal = '';
+        for (let j = 0; j < lines.length; j++) {
+            if (j + i > lines[j].length - 1) {
+                break;
+            }
+            diagonal += lines[j][j + i];
+        }
+        diagonalsNWSE.push(diagonal);
+    }
+    for (const diagonal of diagonalsNWSE) {
+        count += countWordOnLine(diagonal.split(''), word, adjacent);
+        // Reverse
+        count += countWordOnLine(diagonal.split(''), word.split('').reverse().join(''), adjacent);
+    }
     return count;
 };
 
 // Part 1
-//console.log(countWord(lines, 'XMAS', true));
+console.log(countWord(lines, 'XMAS', true));
 
 module.exports = {
     countWord,
