6f349fc79d2cf316bb5795a269b1b8c239456db8
samdouble <samleafs@protonmail.com>
2025-12-22 19:46:31 -0500

    feat(2024.04): count vertically

--- 2024/04/index.js
+++ 2024/04/index.js
@@ -3,30 +3,47 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const lines = input.split('\n');
 
-const countWordOnLine = (chars, word) => {
-    if (word.length === 1) {
-        let regex = new RegExp(word, "g");
-        return (chars.join('').match(regex) || []).length;
-    }
-    let count = 0;
-    for (let i = 0; i < chars.length; i++) {
-        if (chars[i] === word[0]) {
-            count += countWordOnLine(chars.slice(i), word.substring(1))
+const countWordOnLine = (chars, word, adjacent) => {
+    if (!adjacent) {
+        if (word.length === 1) {
+            let regex = new RegExp(word, "g");
+            return (chars.join('').match(regex) || []).length;
         }
+        let count = 0;
+        for (let i = 0; i < chars.length; i++) {
+            if (chars[i] === word[0]) {
+                count += countWordOnLine(chars.slice(i), word.substring(1), adjacent);
+            }
+        }
+        return count;
     }
-    return count;
+    let regex = new RegExp(word, "g");
+    return (chars.join('').match(regex) || []).length;
 };
 
-const countWord = (lines, word) => {
+const countWord = (lines, word, adjacent) => {
     let count = 0;
+    // Horizontally
     for (const line of lines) {
-        count += countWordOnLine(line.split(''), word);
+        count += countWordOnLine(line.split(''), word, adjacent);
+        // Reverse
+        count += countWordOnLine(line.split(''), word.split('').reverse().join(''), adjacent);
+    }
+    // Vertically
+    const columns = lines[0].split('')
+        .map((_c, index) => {
+            return lines.reduce((acc, l) => acc + l.split('')[index], [])
+        });
+    for (const column of columns) {
+        count += countWordOnLine(column.split(''), word, adjacent);
+        // Reverse
+        count += countWordOnLine(column.split(''), word.split('').reverse().join(''), adjacent);
     }
     return count;
 };
 
 // Part 1
-//console.log(countWord(lines, 'XMAS'));
+//console.log(countWord(lines, 'XMAS', true));
 
 module.exports = {
     countWord,
--- 2024/04/test.js
+++ 2024/04/test.js
@@ -14,7 +14,7 @@ describe('countWord', () => {
                 'SAXAMASAAA',
                 'MAMMMXMMMM',
                 'MXMXAXMASX',
-            ], 'XMAS')).toEqual(18);
+            ], 'XMAS', true)).toEqual(18);
     });
 });
 
@@ -30,5 +30,15 @@ describe('countWordOnLine', () => {
         expect(countWordOnLine('SAXAMASAAA'.split(''), 'XMAS')).toEqual(1);
         expect(countWordOnLine('MAMMMXMMMM'.split(''), 'XMAS')).toEqual(0);
         expect(countWordOnLine('MXMXAXMASX'.split(''), 'XMAS')).toEqual(5);
+        expect(countWordOnLine('MMMSXXMASM'.split(''), 'XMAS', true)).toEqual(1);
+        expect(countWordOnLine('MSAMXMSMSA'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('AMXSXMAAMM'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('MSAMASMSMX'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('XMASAMXAMM'.split(''), 'XMAS', true)).toEqual(1);
+        expect(countWordOnLine('XXAMMXXAMA'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('SMSMSASXSS'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('SAXAMASAAA'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('MAMMMXMMMM'.split(''), 'XMAS', true)).toEqual(0);
+        expect(countWordOnLine('MXMXAXMASX'.split(''), 'XMAS', true)).toEqual(1);
     });
 });
