a27f64d72633c70635383d467819b783127836bb
samdouble <samleafs@protonmail.com>
2025-12-24 10:22:46 -0500

    feat(2024.05-2): completed challenge

--- 2024/05/index.js
+++ 2024/05/index.js
@@ -23,7 +23,29 @@ const isUpdateCorrect = (update, graph) => {
     return true;
 };
 
-const getResult = (orders, updates) => {
+const reorder = (update, graph) => {
+    let reorderedUpdate = [];
+    let incorrectlyOrdered = [...update];
+    while (incorrectlyOrdered.length) {
+        for (let i = incorrectlyOrdered.length - 1; i >= 0; i--) {
+            const nodesThatMustGoAfter = graph.get(incorrectlyOrdered[i]) || [];
+            let correctlyPlaced = true;
+            for (let j = 0; j < incorrectlyOrdered.length; j++) {
+                if (nodesThatMustGoAfter.includes(incorrectlyOrdered[j])) {
+                    correctlyPlaced = false;
+                    break;
+                }
+            }
+            if (correctlyPlaced) {
+                reorderedUpdate = [incorrectlyOrdered[i], ...reorderedUpdate];
+                incorrectlyOrdered.splice(i, 1);
+            }
+        }
+    }
+    return reorderedUpdate;
+};
+
+const getGraphFromOrders = (orders) => {
     const graph = new Map();
     for (const order of orders) {
         let before = order[0];
@@ -36,16 +58,33 @@ const getResult = (orders, updates) => {
             after = graph.get(after) || [];
         }
     }
+    return graph;
+};
 
+const getResult = (orders, updates, incorrect = false) => {
+    const graph = getGraphFromOrders(orders);
+
+    if (incorrect) {
+        const incorrectOrderedUpdates = updates
+            .filter(update => !isUpdateCorrect(update, graph));
+        const reorderedUpdates = incorrectOrderedUpdates
+            .map(update => reorder(update, graph));
+        return reorderedUpdates
+            .reduce((acc, update) => acc + Number(update[(update.length - 1) / 2]), 0);
+    }
     const correctOrderedUpdates = updates
         .filter(update => isUpdateCorrect(update, graph));
-    
-    return correctOrderedUpdates.reduce((acc, update) => acc + Number(update[(update.length - 1) / 2]), 0)
+    return correctOrderedUpdates
+        .reduce((acc, update) => acc + Number(update[(update.length - 1) / 2]), 0);
 };
 
 // Part 1
 console.log(getResult(orders, updates));
+// Part 2
+console.log(getResult(orders, updates, true));
 
 module.exports = {
     getResult,
+    getGraphFromOrders,
+    reorder,
 };
